4d32ee7f5dd125035eb7432770f8934638c48cfd
Kisahae <112599839+kisahae@users.noreply.github.com>
2023-10-22 14:09:03 +0700

    Update data.js

--- data.js
+++ data.js
@@ -19,4 +19,32 @@ export let kanji = [
     hint: "Grandfather",
     answer: "祖父"
   },
+  {
+    hint: "Culture",
+    answer: "文化"
+  },
+  {
+    hint:"throw",
+    answer:"投げる"
+  },
+  {
+    hint: "bad",
+    answer: "悪い"
+  },
+  {
+    hint: "Rice Field",
+    answer: "田"
+  },
+  {
+    hint: "World",
+    answer: "世界"
+  },
+  {
+    hint: "word/statement",
+    answer: "言"
+  },
+  {
+    hint: "problem/homework",
+    answer: "問題"
+  },
 ]
