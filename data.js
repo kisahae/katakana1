2655174578e6dad347b44081354a96503a723c62
Kisahae <112599839+kisahae@users.noreply.github.com>
2024-03-26 22:21:45 +0700

    Update data.js

--- data.js
+++ data.js
@@ -1,418 +1,86 @@
 export let kanji = [
   {
-    hint: "Egg",
-    answer: "卵"
-  },
-  {
-    hint: "depart",
-    answer: "発車"
-  },
-  {
-    hint: "Pronunciation",
-    answer: "発音"
-  },
-  {
-    hint: "Doctor",
-    answer: "医者"
-  },
-  {
-    hint: "Age(range)",
-    answer: "代"
-  },
-  {
-    hint: "Grandfather",
-    answer: "祖父"
-  },
-  {
-    hint: "Culture",
-    answer: "文化"
-  },
-  {
-    hint:"throw",
-    answer:"投げる"
-  },
-  {
-    hint: "bad",
-    answer: "悪い"
-  },
-  {
-    hint: "Rice Field",
-    answer: "田"
-  },
-  {
-    hint: "Second (Time)",
-    answer: "秒"
-  },
-  {
-    hint: "Introduce",
-    answer: "紹介"
-  },
-  {
-    hint: "World",
-    answer: "世界"
-  },
-  {
-    hint: "word/statement",
-    answer: "言"
-  },
-  {
-    hint: "problem/homework",
-    answer: "問題"
-  },
-  {
-    hint: "Heart",
-    answer: "心"
-  },
-  {
-    hint: "Ocean/Sea",
-    answer: "海"
-  },
-  {
-    hint: "Sell",
-    answer: "売る"
-  },
-  {
-    hint: "Family",
-    answer: "家族"
-  },
-  {
-    hint: "Mirror",
-    answer: "鏡"
-  },
-  {
-    hint: "Think Over (Again)",
-    answer: "考える"
-  },
-  {
-    hint: "to know",
-    answer: "知る"
-  },
-  {
-    hint: "Heavy",
-    answer: "重い"
-  },
-  {
-    hint: "Light",
-    answer: "軽い"
-  },
-    {
-    hint: "Many",
-    answer: "多い"
-  },
-  {
-    hint: "Temperature",
-    answer: "温度"
-  },
-  {
-    hint: "Arrival",
-    answer: "到着"
+    hint: "Bed",
+    answer: "ベッド"
   },
    {
-    hint: "Wear",
-    answer: "著る"
+    hint: "Towel",
+    answer: "タオル"
   },
   {
-    hint: "Gather",
-    answer: "集まる"
+    hint: "Tooth Brush",
+    answer: "歯ブラシ"
+  },  
+  {
+    hint: "Lettuce",
+    answer: "レタス"
   },
   {
-    hint: "Collect",
-    answer: "集める"
-  },
-    {
-    hint: "Head",
-    answer: "頭"
-  },
-      {
-    hint: "Finger",
-    answer: "指"
-  },
-  {
-    hint: "Teeth",
-    answer: "歯"
-  },
-    {
-    hint: "Face",
-    answer: "顔"
-  },
-      {
-    hint: "Pillow",
-    answer: " 枕"
-  },
-        {
-    hint: "Futon",
-    answer: "布団"
-  },
-    {
-    hint: "Blanket",
-    answer: "毛布"
-  },
-     {
-    hint: "Soap",
-    answer: "石鹸"
-  },
-  {
-    hint: "Bath",
-    answer: "お風呂"
-  },
-  {
-    hint: "Interesting",
-    answer: "面白い"
-  },
-  {
-    hint: "Nephew",
-    answer: "甥"
-  },
-  {
-    hint: "Cousins",
-    answer: "従姉妹"
-  },
-  {
-    hint: "Frog",
-    answer: "蛙"
-  },
-  {
-    hint: "Sushi",
-    answer: "寿司"
-  },
-  {
-    hint: "Magazine",
-    answer: "雑誌"
-  },
-  {
-    hint: "Niece",
-    answer: "姪"
-  },
-  {
-    hint: "Medicine",
-    answer: "薬"
-  },
-  {
-    hint: "Neck",
-    answer: "首"
-  },
-  {
-    hint: "Season",
-    answer: "季節"
-  },
-  {
-    hint: "Winter",
-    answer: "冬"
-  },
-  {
-    hint: "Summer",
-    answer: "夏"
-  },
-  {
-    hint: "Autumn",
-    answer: "秋"
-  },
-  {
-    hint: "Spring",
-    answer: "春"
-  },
-  {
-    hint: "Parents",
-    answer: "親"
-  },
-  {
-    hint: "Paper",
-    answer: "紙"
+    hint: "Banana",
+    answer: "バナナ"
   },
    {
-    hint: "Picture",
-    answer: "画"
-  },
-    {
-    hint: "Night",
-    answer: "夜"
-  },
-  {
-    hint: "Turn oneself info",
-    answer: "化ける"
-  },
-  {
-    hint: "Pull",
-    answer: "引く"
-  },
-    {
-    hint: "To carry/transport",
-    answer: "運ぶ"
-  },
-    {
-    hint: "tercemin/mencerminkan",
-    answer: "映す"
-  },
-    {
-    hint: "Turn/spin",
-    answer: "回す"
-  },
-  {
-    hint: "Lightning",
-    answer: "雷"
+    hint: "Pen",
+    answer: "ペン"
   },
      {
-    hint: "Moss",
-    answer: "苔"
-  },
-  {
-    hint: "Airport",
-    answer: "空港"
-  },
-   {
-    hint: "Ice",
-    answer: "氷"
-  }, 
-    {
-    hint: "Caterpillar",
-    answer: "毛虫"
-  },
-    {
-    hint: "Muscle",
-    answer: "筋肉"
-  },
-    {
-    hint: "Air Force",
-    answer: "空軍"
-  },
-     {
-    hint: "Shell",
-    answer: "貝"
-  },
-     {
-    hint: "Watermelon",
-    answer: "西瓜"
+    hint: "Computer",
+    answer: "コンピューター"
   },
        {
-    hint: "Empty",
-    answer: "空"
+    hint: "Panda",
+    answer: "パンダ"
+  },
+         {
+    hint: "Lion",
+    answer: "ライオン"
+  },
+           {
+    hint: "Orange",
+    answer: "オレンジ"
+  },
+             {
+    hint: "Pink",
+    answer: "ピンク"
+  },
+               {
+    hint: "Cup/Glass",
+    answer: "コップ"
+  },
+  {
+    hint: "Spoon",
+    answer: "スプーン"
+  },
+  {
+    hint: "Fork",
+    answer: "フォーク"
+  },
+    {
+    hint: "Television",
+    answer: "テレビ"
+  },
+    {
+    hint: "Trousers",
+    answer: "ズボン"
+  },
+      {
+    hint: "Lemon",
+    answer: "レモン"
   },
         {
-    hint: "Tree",
-    answer: "木"
+    hint: "Cola",
+    answer: "コーラ"
   },
-  {
-    hint: "Fast (Adjective)",
-    answer: "速い"
+        {
+    hint: "Internet",
+    answer: "インターネット"
   },
-  {
-    hint: "Fast (Adverb)",
-    answer: "早く"
+          {
+    hint: "Bus",
+    answer: "バス"
   },
-   {
-    hint: "A little",
-    answer: "少し"
-  },
-     {
-    hint: "Gate",
-    answer: "門"
-  },
-  {
-    hint: "Road",
-    answer: "道"
-  },
-    {
-    hint: "Station",
-    answer: "駅"
-  },
-      {
-    hint: "behind",
-    answer: "後ろ"
-  },
-  {
-    hint: "West",
-    answer: "西"
-  },
-   {
-    hint: "South",
-    answer: "南"
-  },
-    {
-    hint: "North",
-    answer: "北"
-  },
-      {
-    hint: "Monday",
-    answer: "月曜日"
-  },
-  {
-    hint: "Tuesday",
-    answer: "火曜日"
-  },
-   {
-    hint: "Wednesday",
-    answer: "水曜日"
-  },
-     {
-    hint: "Thursday",
-    answer: "木曜日"
-  },
-  {
-    hint: "Friday",
-    answer: "金曜日"
-  },
-   {
-    hint: "Saturday",
-    answer: "土曜日"
-  },
-     {
-    hint: "Sunday",
-    answer: "日曜日"
-  },
-  {
-    hint: "Maybe",
-    answer: "多分"
-  },
-    {
-    hint: "Sweat",
-    answer: "汗"
-  },
-   {
-    hint: "Currently, at present",
-    answer: "現在"
-  },
-   {
-    hint: "Safety/Security",
-    answer: "安全"
-  },
-   {
-    hint: "Cold",
-    answer: "寒い"
-  },
-  {
-    hint: "Planet",
-    answer: "惑星"
-  },
-   {
-    hint: "Bookshelf",
-    answer: "本棚"
-  },   
-  {
-    hint: "Hobby",
-    answer: "趣味"
-  },
-   {
-    hint: "Envelope",
-    answer: "封筒"
-  },
-     {
-    hint: "Postage Stamp",
-    answer: "切手"
-  },
-  {
-    hint: "Fox",
-    answer: "狐"
-  },
-   {
-    hint: "Plate",
-    answer: "皿"
-  },
-     {
-    hint: "Chopstick",
-    answer: "箸"
-  },
-  {
-    hint: "Refrigerator",
-    answer: "冷蔵庫"
-  },
+            {
+    hint: "Restaurant",
+    answer: "レストラン"
+  }
 ]
